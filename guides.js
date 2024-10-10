5c41f7eacd94e3cab69920ffa00b5e209ed33b48
Maria Blanco <mariaaureliablanco@gmail.com>
2024-10-10 09:17:31 -0300

    Update guides.js

--- guides.js
+++ guides.js
@@ -1,13 +1,11 @@
-  function showPrice(id) {
+ function showPrice(id) {
     var countrySelect = document.getElementById("country-select-" + id).value;
     var argentinaPrice = document.getElementById("argentina-price-" + id);
     var otrosPrice = document.getElementById("otros-price-" + id);
 
-    // Ocultar ambos div por defecto
     argentinaPrice.style.display = "none";
     otrosPrice.style.display = "none";
 
-    // Mostrar el precio según el país seleccionado
     if (countrySelect === "argentina") {
       argentinaPrice.style.display = "block";
     } else if (countrySelect === "otros") {
